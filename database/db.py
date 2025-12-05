16bcfea4e4ba3459c4b54178252498b8b1c1a093
adityadhafa <dtdhafa@gmail.com>
2025-12-05 19:06:59 +0700

    fix(db): build absolute DB_PATH to avoid accidental creation of new nekolog.db

--- database/db.py
+++ database/db.py
@@ -1,9 +1,12 @@
 import sqlite3
+import os 
 
-DB = "nekolog.db"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+DB_PATH = os.path.join(BASE_DIR, "nekolog.db")
 
 def get_connection():
-    conn = sqlite3.connect(DB)
+    conn = sqlite3.connect(DB_PATH)
     
     conn.row_factory = sqlite3.Row
     
