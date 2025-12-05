8e79ee4be6d7811122176629a0bae1e5a6a7aad6
adityadhafa <dtdhafa@gmail.com>
2025-12-05 18:39:40 +0700

    feat: implement Cat model schema and save method

--- models/cat.py
+++ models/cat.py
@@ -1,5 +1,6 @@
+from database.db import get_connection
 class Cat:
-    def __init__(self, name: str, breed: str, weight_kg:str, id=None):
+    def __init__(self, name: str, breed: str, weight_kg:float, id=None):
         self.id = id              
         self.name = name          
         self.breed = breed        
@@ -7,3 +8,38 @@ class Cat:
 
     def __repr__(self):
         return f"<Kucing: {self.name} ({self.breed})>"
+
+    @staticmethod
+    def create_table(): # doesn't need self bcs it's a staticmethod
+        conn = get_connection()
+        cursor = conn.cursor()
+        
+        my_query = """
+        CREATE TABLE IF NOT EXISTS cats (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            breed TEXT,
+            weight_kg REAL
+        )
+        """
+        
+        cursor.execute(my_query)
+        conn.commit()
+        conn.close()
+    
+    def save(self):
+        conn = get_connection()
+        cursor = conn.cursor()
+        
+        my_query= """
+        INSERT INTO cats (name, breed, weight_kg) VALUES (?, ?, ?)
+        """
+        
+        data_tuple = (self.name, self.breed, self.weight_kg)
+        
+        cursor.execute(my_query, data_tuple)
+        
+        self.id = cursor.lastrowid
+        
+        conn.commit()  
+        conn.close()
