4b773073ea1504c3d4fa4e1bf152b654a4b97519
adityadhafa <dtdhafa@gmail.com>
2025-12-08 20:22:18 +0700

    chore: finalize main.py logic connecting all views and models with proper navigation loops

--- main.py
+++ main.py
@@ -1,92 +1,59 @@
+import sys
 from models.cat import Cat
 from models.expense import Expense
 
-from rich.console import Console
-from rich.table import Table
+from view import main_view as mv
+from view import cat_view
+from view import expense_view
+
 
 def main():
-    want = ""
-    while(want != "0"):
-        table = Table(title="=== 🐱 NEKOLOG 🐱 ===")
-        table.add_column("No.",)
-        table.add_column("Menu",)
-        
-        table.add_row("1.", "Add a Cat")
-        table.add_row("2.", "Print All Cats")
-        table.add_row("3.", "Add Expenses")
-        table.add_row("4.", "Expenses Leaderboard")
-        table.add_row("0.", "Exit")
-        
-        console = Console()
-        console.print(table)
-        want = input("Select menu (No.): ")
-        
-        if(want == "2"):
-            res = Cat.get_all()
+    while True:
+
+        ans = mv.display_main_menu()
+
+        if ans == "0":
+            print("Terima kasih sudah menggunakan NekoLog! 😺👋")
+            sys.exit() 
             
-            table = Table(title="All Cats")
-            
-            table.add_column("No.", style="cyan")
-            table.add_column("Id", style="cyan")
-            table.add_column("Name", style="magenta")
-            table.add_column("Breed", style="magenta")
-            table.add_column("Weight (Kg)", style="magenta")
-            
-            for i, cat in enumerate(res, start=1):
-                table.add_row(f"{i}", f"{cat.id}", f"{cat.name}", f"{cat.breed}", f"{cat.weight_kg}")
-            
-            console = Console()
-            console.print(table)
-            
-        elif(want == "3"):
-            amount = int(input("amount: "))
-            
-            name = input("name: ")
-            res = Cat.search_cat(name=name)
-            for i, cat in enumerate(res, start=1):
-                print(f"{i}. id= {cat[0]}, name= {cat[1]}")
-            
-            if res != []:
-                cat_id = int(input("input cat id: "))
-            else:
-                print("salah nama kali")
-                continue
-            
-            category = input("input category: ")
-            date =  input("input date (YYYY-MM-DD): ")
-            description = input("desc: ")
-            
-            new_expenses = Expense(amount=amount, category=category, date=date, description=description, cat_id=cat_id)
-            
-            new_expenses.save()
-            
-            print("expenses saved!")
-            
-        elif(want == "4"):
-            res = Expense.get_leaderboard()
-            
-            table = Table(title="🏆 EXPENSES LEADERBOARD 🏆")
-            
-            table.add_column("No.", style="cyan")
-            table.add_column("Name", style="magenta")
-            table.add_column("Total Expenses", style="magenta")
-            
-            for i, cat in enumerate(res, start=1):
-                table.add_row(f"{i}", f"{cat[0]}", f"{cat[1]}")
-            
-            console = Console()
-            console.print(table)
-            
-        elif(want == "1"):
-            name = input("name: ")
-            breed = input("breed: ")
-            weight = float(input("weight: "))
-            
-            new_cat = Cat(name=name, breed=breed, weight_kg=weight)
-            new_cat.save()
-            
-            print("cat saved!")
-    
+        elif ans == "1":
+            while True: 
+                cat_want = mv.display_cat_submenu()
+                
+                if cat_want == "0":
+                    break 
+                
+                elif cat_want == "1": 
+                    cat_view.add_new_cat_display()
+                    
+                elif cat_want == "2": 
+                    data = Cat.get_all()
+                    cat_view.display_all_cats(data)
+                    
+                elif cat_want == "3": 
+                    cat_view.search_cat_display()
+                    
+                elif cat_want == "4": 
+                    cat_view.update_cat_data_display()
+                    
+                elif cat_want == "5": 
+                    cat_view.delete_cat_display()
+
+        elif ans == "2":
+            while True: 
+                exp_want = mv.display_expense_submenu()
+                
+                if exp_want == "0": 
+                    break
+                
+                elif exp_want == "1": 
+                    expense_view.add_expense_display()
+                    
+                elif exp_want == "2":
+                    data = Expense.get_leaderboard()
+                    
+                    expense_view.display_leaderboard(data)
+
 if __name__ == "__main__":
     main()
     
